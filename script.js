f0483a774811aa4553835a217903e055bb234d0c
unscihsmun25-netizen <unsc.ihsmun25@gmail.com>
2026-02-06 14:02:06 +0400

    Update script.js

--- script.js
+++ script.js
@@ -130,13 +130,13 @@ function drawText() {
 
     if(frameNumber < 250){
         context.fillStyle = `rgba(154, 113, 151, ${opacity})`;
-        context.fillText("everyday day I cannot believe how lucky I am", canvas.width/2, canvas.height/2);
+        context.fillText("everyday I cannot believe how lucky I am", canvas.width/2, canvas.height/2);
         opacity = opacity + 0.01;
     }
     //fades out the text by decreasing the opacity
     if(frameNumber >= 250 && frameNumber < 500){
         context.fillStyle = `rgba(154, 113, 151, ${opacity})`;
-        context.fillText("everyday day I cannot believe how lucky I am", canvas.width/2, canvas.height/2);
+        context.fillText("everyday I cannot believe how lucky I am", canvas.width/2, canvas.height/2);
         opacity = opacity - 0.01;
     }
 
